59e987cf2cd983b873b63f274828a646229a2a8a
faresht <fares.htiouech@sesame.com.tn>
2025-04-28 14:01:26 +0100

    Your commit message

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
---
+++ front/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome Home</h1>
+            <a href="/Register">Go to Register</a>
+        </div>
+    );
+}
+
+export default Home;
